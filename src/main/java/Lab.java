acd08ea2f4c77cd5f50020e9e09aa9eb48b9c4a3
Shannan Scott <Shannanscott102@gmail.com>
2023-03-26 03:29:31 +0000

    Lab.java M

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -41,9 +41,13 @@ public class Lab {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
+        for(int i=start;i<end;i++){
+        buildingString = buildingString + i + " ";
 
-//        code here
+//        
 
-        return buildingString;
+       
     }
+    return buildingString;
+}
 }
